09a059157db3b29e53c341814473c3ce7badf668
Aryaman <aryamansaini5@gmail.com>
2023-12-24 16:18:18 -0600

    Created initial structure for project

---
+++ src/Compiler.java
@@ -0,0 +1,8 @@
+/**
+ * This class is a Java implementation of a LC3 assembly compiler that parses assembly code
+ * to create a LC3 system instance with provided source code
+ */
+
+public class Compiler {
+
+}
---
+++ src/LC3Instance.java
@@ -0,0 +1,12 @@
+/**
+ * This is the instance for an instance of a computer with the LC3 architecture
+ */
+public class LC3Instance {
+    char conditionCode;
+    short programCounter;
+    short instructionRegister;
+    short[] memoryArray = new short[65536];
+    short[] tempRegisters = new short[8];
+
+
+}
